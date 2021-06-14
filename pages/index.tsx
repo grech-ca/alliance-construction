b5cff8cb0150fe8b5389d89d7d1235fbe2ea33ab
Mikhail Grechka <grech.code@gmail.com>
2021-06-14 12:17:34 +0300

    Setup tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
-const HomePage: FC = () => <div>Home</div>;
+const HomePage: FC = () => (
+  <div className="inline-flex justify-center items-center rounded-lg p-4 bg-blue-300">Home</div>
+);
 
 export default HomePage;
